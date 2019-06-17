41666cfe8deb4c3d47efaa6471c296905b7cfdb1
zakprofessional <40375111+zakprofessional@users.noreply.github.com>
2019-06-17 13:10:47 +0000

    popup not working

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
 // "mou9ef.com","mouefcom_root","kalala1998@","mouefcom_mou9ef"
-$link = mysqli_connect("localhost","root","","mou9ef");
+$link = mysqli_connect("localhost","root","","mou9ef-2");
 if(!$link){
     header('location: error/404.html');
     //echo "connecting error";
